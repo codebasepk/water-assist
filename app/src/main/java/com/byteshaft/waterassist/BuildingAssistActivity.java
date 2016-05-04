5fe5cfe4d8c02869384e0844205dede664fcc1f7
s9iper1 <s9iper1@gmail.com>
2016-05-05 00:16:24 +0500

    first commit

---
+++ app/src/main/java/com/byteshaft/waterassist/BuildingAssistActivity.java
@@ -0,0 +1,15 @@
+package com.byteshaft.waterassist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class BuildingAssistActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_building_assist);
+    }
+}
