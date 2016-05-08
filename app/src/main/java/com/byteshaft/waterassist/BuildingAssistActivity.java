6b2652fe4c238e44ad7d04f07a73620ae2aa2cf4
rb-shahid <shgr8rb@gmail.com>
2016-05-08 08:54:49 +0500

    add layout

--- app/src/main/java/com/byteshaft/waterassist/BuildingAssistActivity.java
+++ app/src/main/java/com/byteshaft/waterassist/BuildingAssistActivity.java
@@ -3,17 +3,27 @@ package com.byteshaft.waterassist;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 
 public class BuildingAssistActivity extends AppCompatActivity {
 
-
+    private EditText details;
+    private Button submitButton;
+    private RadioGroup radioGroup;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         getSupportActionBar().setBackgroundDrawable(new
                 ColorDrawable(getResources().getColor(R.color.building_assist_pressed)));
+        super.onCreate(savedInstanceState);
         setTitle("Building Assist");
         setContentView(R.layout.layout_building_assist);
+
+        details = (EditText) findViewById(R.id.building_assist_et);
+        submitButton = (Button) findViewById(R.id.submit_button);
+        radioGroup = (RadioGroup) findViewById(R.id.radio_group);
     }
 }
