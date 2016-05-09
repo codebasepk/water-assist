e66e60ac63a75a5a7b310894f15c6e39d7507eb6
rb-shahid <shgr8rb@gmail.com>
2016-05-09 19:45:29 +0500

    set layout for building and home assist

--- app/src/main/java/com/byteshaft/waterassist/HomeAssistActivity.java
+++ app/src/main/java/com/byteshaft/waterassist/HomeAssistActivity.java
@@ -2,12 +2,43 @@ package com.byteshaft.waterassist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-public class HomeAssistActivity extends Activity {
+public class HomeAssistActivity extends Activity implements RadioGroup.OnCheckedChangeListener, View.OnClickListener {
+
+    RadioGroup radioGroup;
+    EditText details;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_home_assist);
+        radioGroup = (RadioGroup) findViewById(R.id.radio_group_home_assist);
+        details = (EditText) findViewById(R.id.home_assist_et);
+        submitButton = (Button) findViewById(R.id.submit);
+        radioGroup.setOnCheckedChangeListener(this);
+        submitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+        switch (checkedId) {
+            case R.id.home_buyer_survey:
+                break;
+            case R.id.home_buyer_drain_survey:
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.submit:
+                // TODO: 09/05/2016 code for sending email 
+        }
     }
 }
